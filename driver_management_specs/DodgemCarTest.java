dd43fa5619fae163ddfa68fd38448536ea4b4e2d
ben-ghirardani <pretty.fiendish@gmail.com>
2017-08-02 15:58:14 +0100

    drive distance method in DodgemCarTest passing

--- driver_management_specs/DodgemCarTest.java
+++ driver_management_specs/DodgemCarTest.java
@@ -13,7 +13,7 @@ public class DodgemCarTest {
 
   @Test
   public void driveDistanceReturnsTime() {
-    assertEquals(100, car.driveDistance());
+    assertEquals(100, car.driveDistance(10));
   }
 
 
